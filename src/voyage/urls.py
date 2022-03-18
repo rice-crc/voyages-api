b6b68e6e39fc3d2490c451e74c664b57802b77dd
jcm10+1-at-391170303014 <john.connor.mulligan@gmail.com>
2022-03-18 14:49:00 -0500

    minimal test -- voyage duplication endpoint added

--- src/voyage/urls.py
+++ src/voyage/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('geo',views.VoyagePlaceList.as_view()),
     path('aggregations',views.VoyageAggregations.as_view()),
 	path('autocomplete',views.VoyageTextFieldAutoComplete.as_view()),
-	path('insert',views.VoyageInsert.as_view())
+	path('duplicate',views.DuplicateVoyage.as_view())
     ]

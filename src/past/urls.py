2a8294aac41e3a2bbfb99a4f90389403b01ab0fb
jcm10+1-at-391170303014 <john.connor.mulligan@gmail.com>
2022-06-16 10:29:30 -0500

    corrected aggregation and autocomplete endpoints for enslavers

--- src/past/urls.py
+++ src/past/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('enslaved', views.EnslavedList.as_view(), name='tabular'),
-    path('enslaved/autocomplete', views.EnslavedTextFieldAutoComplete.as_view(), name='tabular'),    
-    path('enslaved_aggregations',views.EnslavedAggregations.as_view()),
+    path('enslaved/', views.EnslavedList.as_view()),
+    path('enslaved/autocomplete', views.EnslavedTextFieldAutoComplete.as_view()),    
+    path('enslaved/aggregations',views.EnslavedAggregations.as_view()),
     path('enslavers',views.EnslaverList.as_view()),
-    path('enslavers/autocomplete', views.EnslaverTextFieldAutoComplete.as_view(), name='tabular'),    
+    path('enslavers/autocomplete', views.EnslaverTextFieldAutoComplete.as_view()),    
+    path('enslavers/aggregations',views.EnslaverAggregations.as_view()),
 	path('<int:enslaved_id>/',views.SingleEnslaved.as_view()),
 	path('<int:enslaved_id>/<varname>/',views.SingleEnslavedVar.as_view())
     ]

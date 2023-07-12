2eb4d4643b121fcbb7b2677b47a1b91bb5c9102e
John Connor <jcm10@rice.edu>
2023-07-12 12:26:10 -0500

    admin ui updated per jane hooper prelim requests

--- api/voyage/admin.py
+++ api/voyage/admin.py
@@ -8,6 +8,10 @@ class VoyageCrewInline(admin.StackedInline):
 	model=VoyageCrew
 	max_num=1
 	classes = ['collapse']
+	fields = [
+		'crew_voyage_outset',
+		'crew_died_complete_voyage'
+	]
 
 # 
 
@@ -27,21 +31,26 @@ class VoyageDatesInline(admin.StackedInline):
 		'voyage_completed_sparsedate',
 		'imp_voyage_began_sparsedate',
 		'imp_departed_africa_sparsedate',
-		'imp_arrival_at_port_of_dis_sparsedate'
+		'imp_arrival_at_port_of_dis_sparsedate',
+		'date_departed_africa',
+		'voyage_completed',
+		'imp_departed_africa',
+		'imp_length_home_to_disembark',
+		'imp_length_leaving_africa_to_disembark'
 	]
 	verbose_name_plural="Voyage Dates"
 
-# class PlaceAdmin(admin.ModelAdmin):
-# 	model=Place
-# 	search_fields=['place']
-# 
-# class RegionAdmin(admin.ModelAdmin):
-# 	model=Region
-# 	search_fields=['region']
-# 
-# class BroadRegionAdmin(admin.ModelAdmin):
-# 	model=BroadRegion
-# 	search_fields=['broad_region']
+class PlaceAdmin(admin.ModelAdmin):
+	model=Place
+	search_fields=['place']
+
+class RegionAdmin(admin.ModelAdmin):
+	model=Region
+	search_fields=['region']
+
+class BroadRegionAdmin(admin.ModelAdmin):
+	model=BroadRegion
+	search_fields=['broad_region']
 
 
 
@@ -134,49 +143,53 @@ class VoyageShipInline(admin.StackedInline):
 class VoyageItineraryInline(admin.StackedInline):
 	model = VoyageItinerary
 	max_num = 1
-# 	autocomplete_fields=[
-# 		'imp_broad_region_voyage_begin',
-# 		'port_of_departure',
-# 		'int_first_port_emb',
-# 		'int_third_port_dis',
-# 		'int_fourth_port_dis',
-# 		'int_third_place_region_slave_landing',
-# 		'int_fourth_place_region_slave_landing',
-# 		'int_second_port_emb',
-# 		'int_first_region_purchase_slaves',
-# 		'int_second_region_purchase_slaves',
-# 		'int_first_port_dis',
-# 		'int_second_port_dis',
-# 		'int_first_region_slave_landing',
-# 		'imp_principal_region_slave_dis',
-# 		'int_second_place_region_slave_landing',
-# 		'first_place_slave_purchase',
-# 		'second_place_slave_purchase',
-# 		'third_place_slave_purchase',
-# 		'first_region_slave_emb',
-# 		'second_region_slave_emb',
-# 		'third_region_slave_emb',
-# 		'port_of_call_before_atl_crossing',
-# 		'first_landing_place',
-# 		'second_landing_place',
-# 		'third_landing_place',
-# 		'first_landing_region',
-# 		'second_landing_region',
-# 		'third_landing_region',
-# 		'place_voyage_ended',
-# 		'region_of_return',
-# 		'broad_region_of_return',
-# 		'imp_port_voyage_begin',
-# 		'imp_region_voyage_begin',
-# 		'imp_broad_region_voyage_begin',
-# 		'principal_place_of_slave_purchase',
-# 		'imp_principal_place_of_slave_purchase',
-# 		'imp_principal_region_of_slave_purchase',
-# 		'imp_broad_region_of_slave_purchase',
-# 		'principal_port_of_slave_dis',
-# 		'imp_principal_port_slave_dis',
-# 		'imp_broad_region_slave_dis'
-# 	]
+	autocomplete_fields=[
+		'imp_broad_region_voyage_begin',
+		'port_of_departure',
+		'int_first_port_emb',
+		'int_third_port_dis',
+		'int_fourth_port_dis',
+		'int_third_place_region_slave_landing',
+		'int_fourth_place_region_slave_landing',
+		'int_second_port_emb',
+		'int_first_region_purchase_slaves',
+		'int_second_region_purchase_slaves',
+		'int_first_port_dis',
+		'int_second_port_dis',
+		'int_first_region_slave_landing',
+		'imp_principal_region_slave_dis',
+		'int_second_place_region_slave_landing',
+		'first_place_slave_purchase',
+		'second_place_slave_purchase',
+		'third_place_slave_purchase',
+		'first_region_slave_emb',
+		'second_region_slave_emb',
+		'third_region_slave_emb',
+		'first_landing_place',
+		'second_landing_place',
+		'third_landing_place',
+		'first_landing_region',
+		'second_landing_region',
+		'third_landing_region',
+		'place_voyage_ended',
+		'imp_port_voyage_begin',
+		'principal_place_of_slave_purchase',
+		'imp_principal_place_of_slave_purchase',
+		'imp_principal_region_of_slave_purchase',
+		'imp_broad_region_of_slave_purchase',
+		'principal_port_of_slave_dis',
+		'imp_principal_port_slave_dis',
+		'imp_broad_region_slave_dis'
+	]
+	exclude= [
+		'port_of_call_before_atl_crossing',
+		'number_of_ports_of_call',
+		'region_of_return',
+		'broad_region_of_return',
+		'imp_region_voyage_begin',
+		'imp_broad_region_voyage_begin'
+		
+	]
 	classes = ['collapse']
 # 
 
@@ -225,7 +238,7 @@ class VoyageAdmin(admin.ModelAdmin):
 		VoyageShipInline,
 		VoyageSlavesNumbersInline
 	)
-	fields=['voyage_id','dataset','voyage_groupings','voyage_in_cd_rom']
+	fields=['voyage_id','dataset','voyage_in_cd_rom']
 	list_display=('voyage_id',)
 	search_fields=('voyage_id',)
 	model=Voyage
@@ -233,9 +246,9 @@ class VoyageAdmin(admin.ModelAdmin):
 # 
 admin.site.register(Voyage, VoyageAdmin)
 # admin.site.register(VoyageDates)
-# admin.site.register(Place,PlaceAdmin)
-# admin.site.register(Region,RegionAdmin)
-# admin.site.register(BroadRegion,BroadRegionAdmin)
+admin.site.register(Place,PlaceAdmin)
+admin.site.register(Region,RegionAdmin)
+admin.site.register(BroadRegion,BroadRegionAdmin)
 # admin.site.register(VoyageSources, VoyageSourcesAdmin)
 # admin.site.register(ParticularOutcome, ParticularOutcomeAdmin)
 # admin.site.register(SlavesOutcome, SlavesOutcomeAdmin)
@@ -244,4 +257,4 @@ admin.site.register(Voyage, VoyageAdmin)
 # admin.site.register(Resistance, ResistanceAdmin)
 # admin.site.register(Nationality, NationalityAdmin)
 # admin.site.register(TonType, TonTypeAdmin)
-# admin.site.register(RigOfVessel, RigOfVesselAdmin)
+admin.site.register(RigOfVessel)

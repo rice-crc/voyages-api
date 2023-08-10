e781a1199116f1dc9d0f10d2ca75859c170ef2df
John Connor <jcm10@rice.edu>
2023-08-09 20:47:14 -0500

    new method is faster by ~1 minute

--- api/voyage/management/commands/sync_voyage_dates_data.py
+++ api/voyage/management/commands/sync_voyage_dates_data.py
@@ -11,6 +11,12 @@ from common.models import SparseDate
 class Command(BaseCommand):
 	help = 'Maps all comma-spliced voyage fields to their new counterparts'
 	def handle(self, *args, **options):
+
+###### SHARED BETWEEN METHODS 1 & 2
+##### BOTH OF WHICH TRY TO USE THE BATCH SETTINGS IN DJANGO
+##### WITH A HAND-MADE NUMPY BINNER TO BATCH THE JOB
+##### I ATTEMPTED TO USE THE ASYNC BATCH AND THE REGULAR BATCH SETTINGS IN DJANGO
+######## BUT I COULDN'T GET THE CHANGES TO TAKE WITH EITHER OF THOSE
 		voyagedates=VoyageDates.objects.all()
 		st=time.time()
 		SparseDate.objects.all().delete()
@@ -24,7 +30,8 @@ class Command(BaseCommand):
 		vdfields=[i for i in list(vd1.__dict__.keys()) if i.endswith('_id') and not i=='voyage_id' and i not in excluded_fields]
 		print("vdfields---->",vdfields)
 		
-		#THIS METHOD FINISHES IN ~280 seconds
+		#METHOD 1 -- "LONG" - FIELD BY FIELD OVER ALL 60K+ VOYAGES
+		#FINISHES IN ~280 seconds
 		batch_size=2500
 		print("number of fields:",len(vdfields))
 		for vdfield in vdfields:
@@ -54,8 +61,8 @@ class Command(BaseCommand):
 			print("field finished in %d seconds" %(time.time()-field_st))
 		print("full job finished in %d seconds" %(time.time()-process_st))
 		
-	
-		#AND THIS METHOD FINISHES IN ~350 SECONDS	
+# 		#METHOD 1 -- "WIDE" - ALL FIELDS, VOYAGE-BY-VOYAGE
+# 		#FINISHES IN ~350 seconds
 # 		voyagedate_objs=[]
 # 		batch_size=2500
 # 		print("number of VOYAGEDATES:",len(voyagedates))
@@ -81,5 +88,32 @@ class Command(BaseCommand):
 # 			VoyageDates.objects.bulk_update(batch,vdfields)
 # 			print("batch finished in %d seconds" %(time.time()-st_slice))
 # 		print("full job finished in %d seconds" %(time.time()-process_st))
-		
-		
+
+
+##### METHOD 3 (ORIGINAL) Does not batch.
+		#FINISHES IN ~327 seconds
+# 		voyages=Voyage.objects.all()
+# 
+# 		st=time.time()
+# 		for v in voyages:
+# # 			print(v)
+# 			vd=v.voyage_dates
+# 			vdfields=[i for i in list(vd.__dict__.keys()) if i not in ('_state','id') and not i.endswith('_id')]
+# 			
+# 			for vdfield in vdfields:
+# 				vdval=eval('vd.'+vdfield)
+# 				if "," in str(vdval):
+# 					m,d,y=[int(i) if i!='' else None for i in vdval.split(',')]
+# # 					print(vdfield,[m,d,y])
+# 					sd=SparseDate.objects.create(
+# 						day=d,
+# 						month=m,
+# 						year=y
+# 					)
+# # 					print(sd)
+# 					exec('vd.'+vdfield+'_sparsedate=sd')
+# 			vd.save()
+# # 			print(vd)
+# 		print("now",time.time())
+# 		print("start",st)
+# 		print("completed in %d seconds" %(time.time()-st))

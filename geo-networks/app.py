b5dcd64a41f24ddb448d6788b268b08394941798
John Connor <jcm10@rice.edu>
2023-09-18 09:26:38 -0500

    name error

--- geo-networks/app.py
+++ geo-networks/app.py
@@ -295,7 +295,7 @@ def network_maps():
 	for s in edges:
 		for t in edges[s]:
 			edge=edges[s][t]
-			thisedge={'source':s,'target':t,'w':edge['weight'],'type':edge['type']}
+			thisedge={'source':s,'target':t,'weight':edge['w'],'type':edge['type']}
 			edgesflat.append(thisedge)
 	outputs={
 		"nodes":[nodes[k] for k in nodes],

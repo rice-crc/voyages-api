7b933f4c21c784b5c11ad559cb4286c5ff7aeda8
John Connor <jcm10@rice.edu>
2023-11-05 16:20:11 -0600

    refactored network graphs. much cleaner. last step is to disintermediate transportation-only relations by figuring out roles

--- api/common/urls.py
+++ api/common/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
 	path('global/', views.GlobalSearch.as_view()),
-	path('past_graph/', views.PastGraphMaker.as_view()),
 	path('schemas/', views.Schemas.as_view()),
 # 	path('sparsedate/RD/<int:id>',views.SparseDateRD.as_view())
 ]
--- people-networks/utils.py
+++ people-networks/utils.py
@@ -8,23 +8,19 @@ from networkx_query import search_nodes, search_edges
 import uuid
 import time
 
-def get_or_create_node(G,data_dict):
-	node_class=data_dict['node_class']
-	class_id=data_dict["id"]
-	node_gquery=[{"==":[('node_class',),node_class]},{"==":[('id',),class_id]}]
-	querynodelist=list(search_nodes(G, {"and":node_gquery}))
-	if len(querynodelist)==0:
+def add_nodes(G,long_dataframe,selected_fields,node_class):
+	output_dict={}
+	for row_idx in range(len(long_dataframe[selected_fields[0]])):
+		rowdict={}
+		for sf in selected_fields:
+			rowdict[sf]=long_dataframe[sf][row_idx]
+		id=rowdict['id']
+		rowdict['node_class']=node_class
 		uid=str(uuid.uuid4())
-		G.add_node(uid, **data_dict)
-		node=G.nodes[uid]
-	elif len(querynodelist)==1:
-		uid=querynodelist[0]
-		node=G.nodes[uid]
-	else:
-		print('ruh-roh, we have too many nodes here:',querynodelist)
-		node=None
-		uid=None
-	return node,uid
+		rowdict['uuid']=uid
+		G.add_node(uid, **rowdict)
+		output_dict[id]=rowdict
+	return output_dict
 		
 def load_graph():
 
@@ -56,20 +52,8 @@ def load_graph():
 		data=json.dumps(payload)
 	)
 	
-	voyages_dict={}
-	
 	j=json.loads(r.text)
-	
-	for row_idx in range(len(j[selected_fields[0]])):
-		rowdict={}
-		for sf in selected_fields:
-			rowdict[sf]=j[sf][row_idx]
-		id=rowdict['id']
-		rowdict['node_class']='voyages'
-		uid=str(uuid.uuid4())
-		G.add_node(uid, **rowdict)
-		rowdict['uuid']=uid
-		voyages_dict[id]=rowdict
+	voyages_dict=add_nodes(G,j,selected_fields,'voyages')
 	
 	print("WITH VOYAGES--->",G)
 
@@ -91,20 +75,8 @@ def load_graph():
 		data=json.dumps(payload)
 	)
 	
-	enslavers_dict={}
-	
 	j=json.loads(r.text)
-	
-	for row_idx in range(len(j[selected_fields[0]])):
-		rowdict={}
-		for sf in selected_fields:
-			rowdict[sf]=j[sf][row_idx]
-		id=rowdict['id']
-		rowdict['node_class']='enslavers'
-		uid=str(uuid.uuid4())
-		G.add_node(uid, **rowdict)
-		rowdict['uuid']=uid
-		enslavers_dict[id]=rowdict
+	enslavers_dict=add_nodes(G,j,selected_fields,'enslavers')
 	
 	print("WITH ENSLAVERS-->",G)
 	
@@ -128,20 +100,9 @@ def load_graph():
 		data=json.dumps(payload)
 	)
 	
-	enslaved_dict={}
-	
 	j=json.loads(r.text)
 	
-	for row_idx in range(len(j[selected_fields[0]])):
-		rowdict={}
-		for sf in selected_fields:
-			rowdict[sf]=j[sf][row_idx]
-		id=rowdict['id']
-		rowdict['node_class']='enslaved'
-		uid=str(uuid.uuid4())
-		G.add_node(uid, **rowdict)
-		rowdict['uuid']=uid
-		enslaved_dict[id]=rowdict
+	enslaved_dict=add_nodes(G,j,selected_fields,'enslaved')
 	
 	print("WITH ENSLAVED--->",G)
 		
@@ -201,7 +162,7 @@ def load_graph():
 				G.add_edge(alice,bob)
 			else:
 				print("got more or fewer spouses than anticipated-->",enslaver_uuids,relation_connections)
-		elif relation_type=="transportation":
+		elif relation_type=="Transportation":
 			if len(enslaved_uuids)==0:
 				#captain & shipowner/investor relations to voyages
 				if len(voyage_uuids)==1:
@@ -215,9 +176,10 @@ def load_graph():
 				reldata={
 					'uuid':rel_uuid,
 					'relation_type__name':relation_type,
-					'id':relation_id
+					'id':relation_id,
+					'node_class':'enslavement_relations'
 				}
-				G.add_node(rel_uuid, **rowdict)
+				G.add_node(rel_uuid, **reldata)
 				
 				if voyage_uuids is not None:
 					if len(voyage_uuids)==1:
@@ -235,3 +197,31 @@ def load_graph():
 
 	print("WITH CONNECTIONS-->",G)
 	return (G)
+
+
+
+def add_neighbors(G,nodes_dict,n,edges_list,levels=1):
+	edges=G.edges(n,data=True)
+	selfdata=G.nodes[n]
+	if levels>0:
+		for edge in edges:
+			s,t,edata=edge
+			
+			if s==n:
+				other=t
+			else:
+				other=s
+			otherdata=G.nodes[other]
+			print(s,t,otherdata)
+			other_node_class=otherdata['node_class']
+			if other_node_class=='enslavement_relations':
+				nodes_dict,edges_list=add_neighbors(G,nodes_dict,other,edges_list,levels=levels)
+			else:
+				nodes_dict,edges_list=add_neighbors(G,nodes_dict,other,edges_list,levels=levels-1)
+			s,t=sorted([s,t])
+			e={'source':s,'target':t,'data':edata}
+			if e not in edges_list:
+				edges_list.append(e)
+			nodes_dict[other]=otherdata
+	nodes_dict[n]=selfdata
+	return(nodes_dict,edges_list)

428cda1d7f91ee23342aa493e0b6fe6881b2c5d8
John Connor <unnecessary@github.com>
2024-03-29 11:20:22 -0400

    init

--- api/voyages3/settings.py
+++ api/voyages3/settings.py
@@ -99,7 +99,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.gzip.GZipMiddleware'
 ]
 
 CORS_ALLOW_ALL_ORIGINS=True

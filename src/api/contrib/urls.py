34c5a4cad4a72490097b450df4072c83e48f64ee
Domingos Dellamonica Jr. <domingos@dotproduct.com.br>
2025-05-01 09:18:55 -0300

    Adding an un-filtered geo tree endpoint for contrib.

--- src/api/contrib/urls.py
+++ src/api/contrib/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import batch_data_api
+from .views import batch_data_api, location_tree
 
 urlpatterns = [
     path('data', batch_data_api, name='contrib_data_api'),
+    path('location_tree', location_tree, name='location_tree'),
 ]

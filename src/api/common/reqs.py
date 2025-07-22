e86c99c6aee135b12b31e53b23fc6a8f42daf82e
John Connor <unnecessary@github.com>
2025-07-21 21:24:57 -0400

    updated filterable fields and filter deduping

--- src/api/common/reqs.py
+++ src/api/common/reqs.py
@@ -310,7 +310,6 @@ def post_req(orig_queryset,s,r,options_dict,auto_prefetch=True,paginate=False):
 	st=time.time()
 	pre_order_by_count=filtered_queryset.count()
 	order_by=params.get('order_by')
-	dedupe=False
 	if order_by is not None:
 		if DEBUG:
 			print(f"------>ORDER BY: {order_by}")
@@ -323,15 +322,15 @@ def post_req(orig_queryset,s,r,options_dict,auto_prefetch=True,paginate=False):
 				k=ob
 
 			if k in all_fields:
+				
 				if asc:
 					filtered_queryset=filtered_queryset.order_by(F(k).asc(nulls_last=True))
 				else:
 					filtered_queryset=filtered_queryset.order_by(F(k).desc(nulls_last=True))
+				
 			else:
 				print(f"key is invalid to sort on: {k}")
 				filtered_queryset=filtered_queryset.order_by('id')
-			if "__" in ob:
-				dedupe=True
 	else:
 		filtered_queryset=filtered_queryset.order_by('id')
 	
@@ -346,7 +345,7 @@ def post_req(orig_queryset,s,r,options_dict,auto_prefetch=True,paginate=False):
 	## https://stackoverflow.com/questions/480214/how-do-i-remove-duplicates-from-a-list-while-preserving-order
 	
 # 	if post_order_by_count>pre_order_by_count:
-# 	dedupe=True
+	dedupe=True
 # 	else:
 # 		dedupe=False
 		
